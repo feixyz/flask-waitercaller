8294f1d3fd69262bfa0acd05453aac0df21512f4
Fei <feithu2010@gmail.com>
2017-02-13 14:06:54 +0800

    add bootstrap lib and import flask-login

--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.ext.login import LoginManager, login_required
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 @app.route('/')
 def home():
-    return "under construction"
+    return render_template('home.html')
+
+@app.route('/account')
+@login_required
+def account():
+    return "You are logged in"
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
