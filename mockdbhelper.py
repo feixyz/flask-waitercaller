4bbcc67a85c0279fa758a3bc2ec8a21f92308c1e
lf <feithu2010@gmail.com>
2017-02-13 17:50:53 +0800

    add password hashing, register functionality, change home.html to fit the book

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -1,7 +1,11 @@
-MOCK_USERS = {'test': '123456'}
+MOCK_USERS = [{'email': 'test', 'salt': 'euIMf6MxxN8zN0YBh1yObEkVZfE=', 'hashed': '3ee4a0792c815a22a828642527bed5498f8e22ee134feb8c13882843af636c8747bcfce01e033e293994c11f4f3bdb89907233fb509961129000c92fe9a254af'}]
 
 class MockDBHelper:
     def get_user(self, email):
-        if email in MOCK_USERS:
-            return MOCK_USERS[email]
+        user = [x for x in MOCK_USERS if x.get('email') == email]
+        if user:
+            return user[0]
         return None
+
+    def add_user(self, email, salt, hashed):
+        MOCK_USERS.append({'email': email, 'salt': salt, 'hashed': hashed})
--- waitercaller.py
+++ waitercaller.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_login import LoginManager, login_required, login_user, logout_user
 from user import User
 import config
+from passwordhelper import PasswordHelper
 if config.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
     pass
 
 DB = DBHelper()
+PH = PasswordHelper()
 
 app = Flask(__name__)
 app.secret_key = 'a4e13nbyQdfMo1SGxchbGM8zLj+fCDR9lg07wxFiyVagq1cwdlUlYW3uXSGRXPof'
@@ -22,9 +24,9 @@ def home():
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    user_password = DB.get_user(email)
+    stored_user = DB.get_user(email)
 
-    if user_password and user_password == password:
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user)
         return redirect(url_for('account'))
@@ -39,6 +41,20 @@ def logout():
 @login_required
 def account():
     return "You are logged in"
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form.get('email')
+    pw1 = request.form.get('password')
+    pw2 = request.form.get('password2')
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
 
 @login_manager.user_loader
 def load_user(user_id):
